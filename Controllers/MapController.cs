35dc230fa61e0d1ba410470fbed206020aa4a01f
Victoria-Katherina IGLESIAS <victoria-katherina.iglesias@vinci-energies.net>
2025-01-18 11:00:43 +0100

    Map Init. Drag and drop, douoble click, Inkscape

---
+++ Controllers/MapController.cs
@@ -0,0 +1,33 @@
+﻿using DUNWorkflow.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text.RegularExpressions;
+
+namespace DUNWorkflow.Controllers
+{
+    public class MapController : Controller
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public MapController(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        public IActionResult Map()
+        {
+            // Ruta al archivo JSON (ajusta según la ubicación del archivo)
+            string jsonFileName = "map.json";
+            string jsonFilePath = Path.Combine(_webHostEnvironment.WebRootPath, "json", jsonFileName);
+            string jsonData = System.IO.File.ReadAllText(jsonFilePath);
+
+            // Deserializar el JSON a una lista de objetos Region
+            List<Region> regions = JsonConvert.DeserializeObject<List<Region>>(jsonData);
+
+            ViewBag.RegionsJson = jsonData;
+
+            // Pasar la lista a la vista
+            return View(regions);
+        }
+    }
+}
---
+++ Models/Region.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace DUNWorkflow.Models
+{
+    public class Region
+    {
+        [Key]
+        public string? regionCode { get; set; }
+        public string? info { get; set; }
+        public List<Element>? Elements { get; set; }  // Lista de elementos dentro de la región
+        public List<string>? placesservices { get; set; }
+    }
+
+    public class Element
+    {
+        public string? type { get; set; } 
+    }
+}
