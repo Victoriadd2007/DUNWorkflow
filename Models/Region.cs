4f87b4f0930606c1d55f3b7bdd7f24c56206f8f1
Victoria-Katherina IGLESIAS <victoria-katherina.iglesias@vinci-energies.net>
2025-01-23 10:08:26 +0100

    Map JSON - Adding Region Biomes, Places & Services

--- Models/Region.cs
+++ Models/Region.cs
@@ -8,14 +8,16 @@ namespace DUNWorkflow.Models
         [Key]
         public string? regionCode { get; set; }
         public string? info { get; set; }
-        public List<Element>? Elements { get; set; }  // Lista de elementos dentro de la región
-        public List<Border>? Borders { get; set; }  // Lista de regiones limítrofes
-        public List<string>? placesservices { get; set; }
+        public List<Biomes>? biomes { get; set; }  // Lista de elementos dentro de la región
+        public List<Border>? borders { get; set; }  // Lista de regiones limítrofes
+        
     }
 
-    public class Element
+    public class Biomes
     {
-        public string? type { get; set; } 
+        public string? type { get; set; }
+        public string? alignment { get; set; }
+        public List<string>? placesServices { get; set; }
     }
     public class Border
     {
